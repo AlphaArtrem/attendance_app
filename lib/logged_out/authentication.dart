7a2bb54274fbab33f8ebe91e46118c5c0739d042
AlphaArtrem <yashawasthi01@gmail.com>
2020-03-25 14:01:31 +0530

    Changed the formatting for authentication screen

--- lib/logged_out/authentication.dart
+++ lib/logged_out/authentication.dart
@@ -10,6 +10,7 @@ class _AuthenticationState extends State<Authentication> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: SafeArea(
         child: Center(
           child: Column(
@@ -20,14 +21,28 @@ class _AuthenticationState extends State<Authentication> {
                 onPressed: (){
                   Navigator.of(context).pushNamed('/login');
                 },
-                icon: Icon(Icons.account_circle),
-                label: Text('  Log In  '),
+                icon: Icon(Icons.account_circle, color: Colors.blue, size: 35),
+                label: Text(
+                  '   Log In   ',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+                ),
                 elevation: 0,
+                color: Colors.white,
               ),
               RaisedButton.icon(
-                icon: Icon(Icons.person_add),
-                label: Text('Register'),
+                icon: Icon(Icons.person_add, color: Colors.blue, size: 35),
+                label: Text(
+                  '  Register',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400),
+                ),
                 elevation: 0,
+                color: Colors.white,
                 onPressed: (){
                   Navigator.of(context).pushNamed('/register');
                 }
