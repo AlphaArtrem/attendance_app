2bc7de0e4f6234d46aa5820ece3e39170778fd61
AlphaArtrem <yashawasthi01@gmail.com>
2020-03-31 19:19:52 +0530

    Used EnhancedFutureBuilder to stop filtered list from reseting when setstate is called while searching students

--- lib/logged_in/teacher/add_students.dart
+++ lib/logged_in/teacher/add_students.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+
 import 'package:attendanceapp/classes/account.dart';
 import 'package:attendanceapp/classes/firestore.dart';
 import 'package:attendanceapp/shared/formatting.dart';
+import 'package:enhanced_future_builder/enhanced_future_builder.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -33,77 +36,80 @@ class _AddStudentsState extends State<AddStudents> {
     enrolledStudents = data['enrolledStudents'];
     batch = data['batch'];
     subject = data['subject'];
-    return FutureBuilder(
+    return EnhancedFutureBuilder(
       future: setup(Provider.of<FirebaseUser>(context)),
-      builder: (context, snapshot){
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Add Students',),
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.power_settings_new),
-                onPressed: () async {
-                  dynamic result = await User().signOut();
-                  if (result == null) {
-                    Navigator.of(context).pushReplacementNamed('/authentication');
-                  }
+      rememberFutureResult: true,
+      whenNotDone: LoadingScreen(),
+      whenDone: (arg) => addStudents(),
+    );
+  }
+  Widget addStudents(){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Students',),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.power_settings_new),
+            onPressed: () async {
+              dynamic result = await User().signOut();
+              if (result == null) {
+                Navigator.of(context).pushReplacementNamed('/authentication');
+              }
+            },
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 20, 15, 2),
+              child: TextFormField(
+                decoration: textInputFormatting.copyWith(hintText: 'Search Student By Email'),
+                onChanged: (val){
+                  setState(() {
+                    filteredStudents = allStudents.where((student) => student.toLowerCase().contains(val.toLowerCase())).toList();
+                  });
                 },
-              )
-            ],
-          ),
-          body: allStudents.isEmpty ? LoadingScreen() : Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Form(
-                key: _formKey,
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 20, 15, 2),
-                  child: TextFormField(
-                    decoration: textInputFormatting.copyWith(hintText: 'Search Student By Email'),
-                    onChanged: (val){
-                      setState(() {
-                        filteredStudents = allStudents.where((student) => student.toLowerCase().contains(val.toLowerCase())).toList();
-                      });
-                    },
-                  ),
-                ),
               ),
-              SizedBox(height: 15),
-              Text(message, style: TextStyle(color: Colors.red),),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
-                  child: ListView.builder(
-                    itemBuilder: (context, index){
-                      return Card(
-                        child: ListTile(
-                          onTap: () async{
-                              dynamic result = await _tSAB.addStudent(subject, batch, filteredStudents[index]);
-                              if(result == 'Success'){
-                                setState(() {
-                                  enrolledStudents.add(filteredStudents[index]);
-                                  filteredStudents.remove(filteredStudents[index]);
-                                  Navigator.pop(context, {'enrolledStudents' : enrolledStudents,});
-                                });
-                              }
-                              else{
-                                setState(() {
-                                  message = "Something Went Wrong Couldn't Add Student";
-                                });
-                              }
-                          },
-                          title: Text('${filteredStudents[index]}'),
-                        ),
-                      );
-                    },
-                    itemCount: filteredStudents.length,),
-                ),
-              ),
-            ],
+            ),
           ),
-        );
-      },
+          SizedBox(height: 15),
+          Text(message, style: TextStyle(color: Colors.red),),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+              child: ListView.builder(
+                itemBuilder: (context, index){
+                  return Card(
+                    child: ListTile(
+                      onTap: () async{
+                        dynamic result = await _tSAB.addStudent(subject, batch, filteredStudents[index]);
+                        if(result == 'Success'){
+                          setState(() {
+                            enrolledStudents.add(filteredStudents[index]);
+                            filteredStudents.remove(filteredStudents[index]);
+                            Navigator.pop(context, {'enrolledStudents' : enrolledStudents,});
+                          });
+                        }
+                        else{
+                          setState(() {
+                            message = "Something Went Wrong Couldn't Add Student";
+                          });
+                        }
+                      },
+                      title: Text('${filteredStudents[index]}'),
+                    ),
+                  );
+                },
+                itemCount: filteredStudents.length,),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
