b069e227c3f44e5b8f831487497c4284b16e2240
AlphaArtrem <yashawasthi01@gmail.com>
2020-04-14 17:19:37 +0530

    Changed student list screen top

--- lib/logged_in/teacher/students.dart
+++ lib/logged_in/teacher/students.dart
@@ -34,59 +34,114 @@ class _EnrolledStudentsState extends State<EnrolledStudents> {
     subject = data['subject'];
     batch = data['batch'];
     return Scaffold(
-      appBar: AppBar(
-        title: batch.isEmpty && subject.isEmpty ? Text('Students',) : Text(
-          '$subject [$batch]',),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.power_settings_new),
-            onPressed: () async {
-              dynamic result = await User().signOut();
-              if (result == null) {
-                Navigator.of(context).pushReplacementNamed('/authentication');
-              }
-            },
-          )
-        ],
-      ),
-      body: FutureBuilder(
-          future: setup(Provider.of<FirebaseUser>(context), subject, batch),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            return students.isEmpty ? LoadingData() : Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        body: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: Stack(
                 children: <Widget>[
-                  addStudentButton(),
-                  SizedBox(height: 10,),
-                  students[0] == 'Empty' ? Expanded(
-                    child: Text('You Need To Add Students',
-                      style: TextStyle(color: Colors.red),),
-                  ) : Expanded(
-                    child: ListView.builder(
-                      itemCount: students.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                            child: ListTile(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/attendanceList', arguments: {
-                                  'teacherEmail' : Provider.of<FirebaseUser>(context, listen: false).email ,
-                                  'subject': subject,
-                                  'batch' : batch,
-                                  'studentEmail' : students[index],
-                                });
-                              },
-                              title: Text('${students[index]}'),
-                            )
-                        );
-                      },
+                  Container(
+                    padding: EdgeInsets.fromLTRB(45, 60, 30, 50),
+                    decoration: BoxDecoration(
+                        color: Colors.cyan,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50)
+                        )
                     ),
-                  )
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Text('Students', style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),)),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(Radius.circular(50))
+                          ),
+                          child: FlatButton.icon(
+                            label: Text('Log Out', style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold)),
+                            icon: Icon(Icons.exit_to_app, color: Colors.cyan, size: 15,),
+                            onPressed: () async {
+                              dynamic result = await User().signOut();
+                              if (result == null) {
+                                Navigator.of(context).pushReplacementNamed('/authentication');
+                              }
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(40, 130, 40, 20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [BoxShadow(
+                        color: Color.fromRGBO(51, 204, 255, 0.3),
+                        blurRadius: 10,
+                        offset: Offset(0, 10),
+                      )],
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(6.5),
+                      child: TextFormField(
+                        decoration: authInputFormatting.copyWith(hintText: "Search By ID"),
+                        onChanged: (val){
+                          setState(() {
+                          });
+                        },
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            );
-          }
-      ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.white,
+                child: FutureBuilder(
+                    future: setup(Provider.of<FirebaseUser>(context), subject, batch),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      return students.isEmpty ? LoadingData() : Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            addStudentButton(),
+                            SizedBox(height: 10,),
+                            students[0] == 'Empty' ? Expanded(
+                              child: Text('You Need To Add Students',
+                                style: TextStyle(color: Colors.red),),
+                            ) : Expanded(
+                              child: ListView.builder(
+                                itemCount: students.length,
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                      child: ListTile(
+                                        onTap: () {
+                                          Navigator.pushNamed(context, '/attendanceList', arguments: {
+                                            'teacherEmail' : Provider.of<FirebaseUser>(context, listen: false).email ,
+                                            'subject': subject,
+                                            'batch' : batch,
+                                            'studentEmail' : students[index],
+                                          });
+                                        },
+                                        title: Text('${students[index]}'),
+                                      )
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                ),
+              ),
+            ),
+          ],
+        )
     );
   }
 
@@ -110,7 +165,7 @@ class _EnrolledStudentsState extends State<EnrolledStudents> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Icon(Icons.add),
-                    SizedBox(width: 10,),
+                    SizedBox(width: 5,),
                     Text('Student')
                   ],
                 ),
@@ -127,7 +182,7 @@ class _EnrolledStudentsState extends State<EnrolledStudents> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Icon(Icons.add),
-                    SizedBox(width: 10,),
+                    SizedBox(width: 5,),
                     Text('Attendance')
                   ],
                 ),
