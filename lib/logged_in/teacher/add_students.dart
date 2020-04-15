27461756deab5fa51bd0b6cbe013a4b02a4330ab
AlphaArtrem <yashawasthi01@gmail.com>
2020-04-15 15:36:16 +0530

    Changed formatting for add stduent screen

--- lib/logged_in/teacher/add_students.dart
+++ lib/logged_in/teacher/add_students.dart
@@ -1,6 +1,4 @@
 import 'dart:ui';
-
-import 'package:attendanceapp/classes/account.dart';
 import 'package:attendanceapp/classes/firestore.dart';
 import 'package:attendanceapp/shared/formatting.dart';
 import 'package:enhanced_future_builder/enhanced_future_builder.dart';
@@ -15,27 +13,26 @@ class AddStudents extends StatefulWidget {
 }
 
 class _AddStudentsState extends State<AddStudents> {
-  final _formKey = GlobalKey<FormState>();
-  List<String> filteredStudents, enrolledStudents;
-  List<String> allStudents = [];
-  String message = ' ';
-  String batch, subject;
-  final StudentsList _allStudents = StudentsList();
+  List<String> _filteredStudents, _enrolledStudents;
+  List<String> _allStudents = [];
+  String _message = ' ';
+  String _batch, _subject;
+  final StudentsList _allStudentsInstance = StudentsList();
   TeacherSubjectsAndBatches _tSAB;
 
   Future setup(FirebaseUser user) async{
     _tSAB = TeacherSubjectsAndBatches(user);
-    allStudents = await _allStudents.getAllStudents();
-    allStudents = allStudents.where((student) => !enrolledStudents.contains(student)).toList();
-    filteredStudents = allStudents;
+    _allStudents = await _allStudentsInstance.getAllStudents();
+    _allStudents = _allStudents.where((student) => !_enrolledStudents.contains(student)).toList();
+    _filteredStudents = _allStudents;
   }
 
   @override
   Widget build(BuildContext context){
     Map data = ModalRoute.of(context).settings.arguments;
-    enrolledStudents = data['enrolledStudents'];
-    batch = data['batch'];
-    subject = data['subject'];
+    _enrolledStudents = data['enrolledStudents'];
+    _batch = data['batch'];
+    _subject = data['subject'];
     return EnhancedFutureBuilder(
       future: setup(Provider.of<FirebaseUser>(context)),
       rememberFutureResult: true,
@@ -45,70 +42,81 @@ class _AddStudentsState extends State<AddStudents> {
   }
   Widget addStudents(){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Students',),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.power_settings_new),
-            onPressed: () async {
-              dynamic result = await User().signOut();
-              if (result == null) {
-                Navigator.of(context).pushReplacementNamed('/authentication');
-              }
-            },
-          )
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Form(
-            key: _formKey,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(15, 20, 15, 2),
-              child: TextFormField(
-                decoration: textInputFormatting.copyWith(hintText: 'Search Student By Email'),
-                onChanged: (val){
-                  setState(() {
-                    filteredStudents = allStudents.where((student) => student.toLowerCase().contains(val.toLowerCase())).toList();
-                  });
-                },
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                  color: Color.fromRGBO(51, 204, 255, 0.3),
+                  blurRadius: 10,
+                  offset: Offset(0, 10),
+                )],
+              ),
+              child: Container(
+                padding: EdgeInsets.all(6.5),
+                child: Row(
+                  children: <Widget>[
+                    BackButton(color: Colors.grey[700],),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: authInputFormatting.copyWith(hintText: "Search Student By Email"),
+                        onChanged: (val){
+                          setState(() {
+                            _filteredStudents = _allStudents.where((student) => student.toLowerCase().contains(val.toLowerCase())).toList();
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(height: 15),
-          Text(message, style: TextStyle(color: Colors.red),),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
-              child: ListView.builder(
-                itemBuilder: (context, index){
-                  return Card(
-                    child: ListTile(
-                      onTap: () async{
-                        dynamic result = await _tSAB.addStudent(subject, batch, filteredStudents[index]);
-                        if(result == 'Success'){
-                          setState(() {
-                            enrolledStudents.add(filteredStudents[index]);
-                            filteredStudents.remove(filteredStudents[index]);
-                            Navigator.pop(context, {'enrolledStudents' : enrolledStudents,});
-                          });
-                        }
-                        else{
-                          setState(() {
-                            message = "Something Went Wrong Couldn't Add Student";
-                          });
-                        }
-                      },
-                      title: Text('${filteredStudents[index]}'),
-                    ),
-                  );
-                },
-                itemCount: filteredStudents.length,),
+            Center(child: Text(_message, style: TextStyle(color: Colors.red),)),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 20),
+                child: ListView.builder(
+                  itemBuilder: (context, index){
+                    return Card(
+                      elevation: 1,
+                      child: Container(
+                        padding: EdgeInsets.all(6.5),
+                        child: ListTile(
+                          onTap: () async{
+                            dynamic result = await _tSAB.addStudent(_subject, _batch, _filteredStudents[index]);
+                            if(result == 'Success'){
+                              setState(() {
+                                _enrolledStudents.add(_filteredStudents[index]);
+                                _filteredStudents.remove(_filteredStudents[index]);
+                                Navigator.pop(context, {'enrolledStudents' : _enrolledStudents,});
+                              });
+                            }
+                            else{
+                              setState(() {
+                                _message = "Something Went Wrong Couldn't Add Student";
+                              });
+                            }
+                          },
+                          title: Row(
+                            children: <Widget>[
+                              Expanded(child: Text('${_filteredStudents[index]}', style: TextStyle(color: Colors.cyan),)),
+                              Icon(Icons.add_circle_outline, color: Colors.blueGrey,)
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: _filteredStudents.length,),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
